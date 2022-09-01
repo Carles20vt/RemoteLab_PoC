a7a29ae72503d39ba6d0dc25e75e72d808b93e0d
Carles Arasil <carles.arasil@contractors.roche.com>
2022-09-01 18:54:47 +0200

    Using Interaction Toolkit with XR Universal. Fixed grab

--- Assets/Scripts/XRGrabNetworkInteractable.cs
+++ Assets/Scripts/XRGrabNetworkInteractable.cs
@@ -2,24 +2,40 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(PhotonView), typeof(Rigidbody))]
 public class XRGrabNetworkInteractable : XRGrabInteractable
 {
 
     #region Private Properties
 
     private PhotonView photonView;
+    private Rigidbody interactableRigidbody;
 
     #endregion
     
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
+        interactableRigidbody = GetComponent<Rigidbody>();
     }
 
     protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
         photonView.RequestOwnership();
         base.OnSelectEntering(args);
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        interactableRigidbody.isKinematic = true;
+        interactableRigidbody.detectCollisions = false;
+        base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        interactableRigidbody.isKinematic = false;
+        interactableRigidbody.detectCollisions = true;
+        base.OnSelectExited(args);
     }
 }
